6c666a2e13d62d8b0a86c266d1d09a108f969012
Karl Heitmann <khf1988@gmail.com>
2021-03-27 03:12:36 -0300

    candlestick example agregado

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Plot from 'react-plotly.js';
+import {data, layout} from './candle_stick_example'
 
 function Home(props) {
   const [currentTime, setCurrentTime] = useState(0);
@@ -27,21 +28,24 @@ function Home(props) {
 
   console.log(candles);
 
+
   return <>
     <h1>Home</h1>
     <button onClick={clickCandles}>Candles</button>
     <p>The current time is {currentTime}.</p>
     <Plot
-      data={[
-        {
-          y: candles,
-          type: 'scatter',
-          mode: 'lines+markers',
-          marker: {color: 'red'},
-        },
-      ]}
-      // layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
-      layout={ {title: 'A Fancy Plot'} }
+      data={data}
+      layout={layout}
+      // data={[
+      //   {
+      //     y: candles,
+      //     type: 'scatter',
+      //     mode: 'lines+markers',
+      //     marker: {color: 'red'},
+      //   },
+      // ]}
+      // // layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
+      // layout={ {title: 'A Fancy Plot'} }
     />
     <table>
       {candles.map(candle => <tr>
