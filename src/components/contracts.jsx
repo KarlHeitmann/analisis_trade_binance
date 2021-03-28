3c428d096c1d7e5699e374ff2f2dda12637e2b1a
Karl Heitmann <khf1988@gmail.com>
2021-03-28 00:16:53 -0300

    colocada una tabla

--- src/components/contracts.jsx
+++ src/components/contracts.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { Table, Space } from 'antd';
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: text => <a>{text}</a>,
+  },
+];
+
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+  },
+];
 
 function Contracts(props) {
-  return <h1>Contracts</h1>;
+  return <Table columns={columns} dataSource={data} />;
 }
 
 export default Contracts;
