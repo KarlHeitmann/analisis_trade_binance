22ebc5fb7d9a9b1e636ca2b70ec6a21a214e381b
Karl Heitmann <khf1988@gmail.com>
2021-03-27 23:57:50 -0300

    agregado otro binance_futures

--- api/api.py
+++ api/api.py
@@ -2,63 +2,31 @@ import time
 import os
 from flask import Flask
 
-from bot.connectors.binance_futures import BinanceFuturesClient
-from bot.strategies import BreakoutStrategy
+# from bot.connectors.binance_futures import BinanceFuturesClient
+# from bot.strategies import BreakoutStrategy
 
+from binance_futures import BinanceFuturesClient
 
 app = Flask(__name__)
+binance = BinanceFuturesClient(os.environ['BINANCE_KEY'], os.environ['BINANCE_SECRET'], True, 'cut')
+contracts = binance.get_contracts()
 
 time_frame = '1m'
 symbol = 'BTCUSDT'
-binance = None
-contracts = None
-contract = None
-strategy = None
-
-other_param = {
-    'min_volume': 0
-}
-
-@app.route('/start')
-def get_start():
-    print("START")
-    global binance, contracts, contract, strategy
-    binance = BinanceFuturesClient(os.environ['BINANCE_KEY'], os.environ['BINANCE_SECRET'], True, 'cut')
-    contracts = binance.get_contracts()
-    contract = contracts[symbol]
-    strategy = BreakoutStrategy(binance, contract, 'binance', time_frame, 0.1, 0.1, 0.1, other_param)
-    strategy.candles = binance.get_historical_candles(contract, time_frame)
-    binance.strategies[symbol] = strategy
-    binance.strategies[0] = strategy
-    print("END")
-    return {'ready': True}
 
 @app.route('/candles')
 def get_candles():
-    global strategy
-    print("WAWAWAWWAWAWAWAWA")
-    print("000000000000000000000")
-    print("---------------------------------")
-
-    print("::::::::::::::::::::::::::::::::::::::::::.")
-
+    global binance, contracts
     headers = ['close', 'open', 'high', 'low']
-    # candles = {'close': [], 'open': [], 'high': [], 'low': []}
-
-    # candles = [ {'close': candle.close, 'open': candle.open, 'high': candle.high, 'low': candle.low, 'volume': candle.volume} for candle in strategy.candles ]
-
-    # candles = dict()
-    # for header in headers:
-    #     candles[header] = [  ]
-    print("STRATEGY----------------", strategy)
-    print(strategy.candles[0])
+    print("STRATEGY----------------", binance)
+    candles = binance.get_contracts(contracts['BTCUSDT', '1m'])
     candles = {}
-    candles['close'] = [ candle.close for candle in strategy.candles ]
-    candles['open'] = [ candle.open for candle in strategy.candles ]
-    candles['high'] = [ candle.high for candle in strategy.candles ]
-    candles['low'] = [ candle.low for candle in strategy.candles ]
-    candles['volume'] = [ candle.volume for candle in strategy.candles ]
-    candles['timestamp'] = [ candle.timestamp for candle in strategy.candles ]
+    candles['close'] = [ candle.close for candle in candles ]
+    candles['open'] = [ candle.open for candle in candles ]
+    candles['high'] = [ candle.high for candle in candles ]
+    candles['low'] = [ candle.low for candle in candles ]
+    candles['volume'] = [ candle.volume for candle in candles ]
+    candles['timestamp'] = [ candle.timestamp for candle in candles ]
 
 
     return {'candles': candles}
