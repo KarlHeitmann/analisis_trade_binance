5e66aaaa1b0d0e03b7119449af6a7225cfc080a1
Karl Heitmann <khf1988@gmail.com>
2021-03-28 02:06:51 -0300

    muestra los symbols en la tabla

--- src/components/contracts.jsx
+++ src/components/contracts.jsx
@@ -1,45 +1,22 @@
 import React, {useEffect, useState} from 'react';
 import { Table, Space } from 'antd';
 
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-    render: text => <a>{text}</a>,
-  },
-];
-
-const data = [
-  {
-    key: '1',
-    name: 'John Brown',
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-  },
-];
-
 function Contracts(props) {
   const [symbols, setSymbols] = useState([])
+  const columns = [
+    {
+      title: 'Symbol',
+      dataIndex: 'symbol',
+      key: 'symbol',
+      render: text => <a>{text}</a>,
+    },
+  ];
+
   useEffect(() => {
     fetch('/symbols').then(res => res.json()).then(data => {
-      const {symbols} = data;
-      // symbols = symbols.map((symbol, i) => {"key": i, "name": symbol})
-      const ss = symbols.map((currElement, index) => {
-        console.log("The current iteration is: " + index);
-        console.log("The current element is: " + currElement);
-        console.log("\n");
-        return {key: index, name: currElement}; //equivalent to list[index]
-      });
-      console.log("symbols", symbols);
-      console.log("ss", ss);
-      setSymbols(ss)
+      let {symbols} = data;
+      symbols = symbols.map((symbol, index) => { return {key: index, symbol: symbol}; });
+      setSymbols(symbols)
     });
   }, []);
 
